9d42f5c81e612490c69ae9b5602187d0ee1606bb
Ryan P. Hill <rphill299@gmail.com>
2024-03-15 21:29:09 -0400

    connect react-ui to flask-backend for basic parameters/results passing

--- flask-server/src/server.py
+++ flask-server/src/server.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+import sys
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type' #this line may or may not be unnecessary
+FILE = sys.stderr #print to this file within endpoints to print to console
 
-
-@app.route('/test')
-def hello():
-    return {'result', 'Hello, World!'}
+@app.route('/test/')
+@cross_origin()
+def helloName():
+    print("received request", file=FILE)
+    name = request.args.get('name')
+    name = name.upper()
+    response = {'result':'Hello, ' + name + '!'}
+    return response
