8826b7a843b853e9b7c5521baeb1b8a506c74cc0
Matt Chase <mnchase@umass.edu>
2024-03-20 22:38:24 -0400

    Actually adding the backend.

--- flask-server/src/server.py
+++ flask-server/src/server.py
@@ -1,17 +1,21 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import sys
+from wav2vec2_test2 import run
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type' #this line may or may not be unnecessary
 FILE = sys.stderr #print to this file within endpoints to print to console
 
-@app.route('/test/')
+@app.route('/test/', methods = ['POST'])
 @cross_origin()
 def helloName():
     print("received request", file=FILE)
-    name = request.args.get('name')
-    name = name.upper()
-    response = {'result':'Hello, ' + name + '!'}
+    file = request.files['file']
+   # print(name)
+   # file = request.args.get('file')
+   # file.read()
+    transcript = run(file=file, flag=False)
+    response = {'result':transcript}
     return response
