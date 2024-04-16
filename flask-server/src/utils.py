0976bb8071ee9341c65c5dd090728ff4b9be1630
mnchase <160336885+mnchase@users.noreply.github.com>
2024-04-16 17:33:19 -0400

    Update utils.py
    Added print for testing

--- flask-server/src/utils.py
+++ flask-server/src/utils.py
@@ -8,4 +8,5 @@ def read_file(file):
     data, sr = librosa.load(buf)
     if sr != 16000:
         data = librosa.resample(data, orig_sr=sr, target_sr=16000)
+    print(data[:100])
     return data
