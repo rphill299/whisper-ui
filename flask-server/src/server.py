4cb160fe1dd6bd4550be368a96ac02d827bba613
Matt Chase <mnchase@umass.edu>
2024-04-26 17:16:33 -0400

    Deleted extraneous HTTP calls (both calls dealing with batched whisper calls and the wav2vec2 call).

--- flask-server/src/server.py
+++ flask-server/src/server.py
@@ -35,69 +35,6 @@ def init():
     outputDir = join(CURR_DIR, "outputs")
     response = { 'outputFolder' : outputDir } 
     return response
-
-
-# transcribe a single file with Whisper
-@app.route('/whisper-transcribe-file/', methods = ['GET'])
-@cross_origin()
-def whisper_transcribe_file():
-    inputFilename = request.args.get("filename")
-    inputFolder = request.args.get("folder")
-    inputFilePath = join(inputFolder, inputFilename)
-    print("received whisper transcribe request for " + inputFilename, file=PRINT_TO_CONSOLE)
-
-    transcript = model.transcribe(inputFilePath)
-
-    response = {'status'    : 0,
-                'transcript': transcript}
-    return response
-
-# transcribe a multiple files with Batched Whisper
-@app.route('/whisper-transcribe-files-batched/', methods = ['GET'])
-@cross_origin()
-def whisper_transcribe_file_batched():
-    print("received batched whisper transcribe request", file=PRINT_TO_CONSOLE)
-    files_json = request.args.get("filenames")
-
-    if files_json:
-        filenames = json.loads(files_json) # Parse JSON string back into Python list
-    else:
-        filenames = []
-    inputFolder = request.args.get("folder")
-    inputFilePaths = [join(inputFolder, inputFilename) for inputFilename in filenames]
-
-    # if no GPU, sequential transcribe
-    if not torch.cuda.is_available() : 
-        transcripts = []
-        for ifp in inputFilePaths :
-            transcripts.append(model.transcribe(ifp)['text'])
-
-        # check if saving outputs and save to folder 
-        if request.args.get('saveOutputs') :
-            of = request.args.get('outputFolder')
-            saveTextOutputs(of, [join(of, f.split('.')[0]+"_output.txt") for f in filenames], transcripts)
-    
-        return {'status': 0, 'transcript':transcripts}
-
-    
-    ds = load_dataset(inputFolder, data_files=filenames)["train"]
-    ds = ds.cast_column("audio", Audio(sampling_rate=16000))
-    raw_audio = [x["array"].astype(np.float32) for x in ds["audio"]]
-
-    # process input, make sure to pass `padding='longest'` and `return_attention_mask=True`
-    processor = AutoProcessor.from_pretrained("openai/whisper-medium.en")
-    inputs = processor(raw_audio, return_tensors="pt", truncation=False, padding="longest", return_attention_mask=True, sampling_rate=16_000)
-    inputs = inputs.to("cuda", torch.float16)
-    model_medium = WhisperForConditionalGeneration.from_pretrained("openai/whisper-medium.en", torch_dtype=torch.float16)
-    model_medium.to("cuda")
-
-    # activate `temperature_fallback` and repetition detection filters and condition on prev text
-    result = model_medium.generate(**inputs, condition_on_prev_tokens=False, temperature=(0.0, 0.2, 0.4, 0.6, 0.8, 1.0), logprob_threshold=-1.0, compression_ratio_threshold=1.35, return_timestamps=True)
-
-    transcript = processor.batch_decode(result, skip_special_tokens=True)
-    response = {'status'    : 0,
-                'transcript': transcript}
-    return response
     
 @app.route('/transcribe/', methods = ['POST'])
 @cross_origin()
@@ -129,16 +66,4 @@ def transcribe():
                 'transcript': transcripts}
     if request.args.get('saveOutputs') == 'true' :
         saveTextOutputs(request.args.get('outputFolder'), filepaths, transcripts)
-    return response
-
-@app.route('/wav2vec2-transcribe/', methods = ['POST'])
-@cross_origin()
-def wav2vec2_transcribe():
-    for _file in request.files.values():
-        print("received wav2vec2 transcribe request for " + _file.filename, file=PRINT_TO_CONSOLE)
-        audio = loadAudio(_file)
-        transcript = model.transcribe(audio)
-        print(transcript['text'], file=PRINT_TO_CONSOLE)
-    response = {'status'    : 0,
-                'transcript':'4'}
     return response
